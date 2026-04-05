2e74792d5ca35fd4d9cb34c2c71972dcc209177a
Chus <wurrzag16@gmail.com>
2026-04-05 16:00:31 +0200

    Cambios menores en moveCategory.js y en Input.js. Bolognesa terminada.

--- src/dataTemplate/fighters/moves/moveCategory.js
+++ src/dataTemplate/fighters/moves/moveCategory.js
@@ -4,12 +4,15 @@
  * @description Categorías de movimientos de un personaje.
  * - `UNIVERSAL`: Movimiento disponible para todos los personajes.
  * - `NORMAL`: Movimiento estándar, se hacen con una dirección.
+ * - `JUMP`: Movimiento aereo, debe estar en el aire para poderse realizar.
  * - `SPECIAL`: Movimiento especial con *motion input*.
  * - `OVERDRIVE`: Movimiento especial poderoso con requisitos adicionales o *motion input* complejo.
  */
 export const MoveCategory = Object.freeze({
-    UNIVERSAL: "universal",
-    NORMAL: "normal",
-    SPECIAL: "special",
-    OVERDRIVE: "overdrive",
+  UNIVERSAL: "universal",
+  NORMAL: "normal",
+  JUMP: "jump",
+  SPECIAL: "special",
+  OVERDRIVE: "overdrive",
 });
+
