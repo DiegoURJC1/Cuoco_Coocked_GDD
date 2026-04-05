bcb46fba5fb5796e6a5dbb6567f7321ede73921e
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-04-06 00:52:55 +0200

    Revert inputs
    Make grabs part of -> dust and <- dust.

--- src/dataTemplate/input/enums/Input.js
+++ src/dataTemplate/input/enums/Input.js
@@ -8,23 +8,23 @@
  * @enum {string}
  */
 export const Input = Object.freeze({
-  UP_LEFT: "7",
-  UP: "8",
-  UP_RIGHT: "9",
-  LEFT: "4",
-  NEUTRAL: "5",
-  RIGHT: "6",
-  DOWN_LEFT: "1",
-  DOWN: "2",
-  DOWN_RIGHT: "3",
+    UP_LEFT: "7",
+    UP: "8",
+    UP_RIGHT: "9",
+    LEFT: "4",
+    NEUTRAL: "5",
+    RIGHT: "6",
+    DOWN_LEFT: "1",
+    DOWN: "2",
+    DOWN_RIGHT: "3",
 
-  // Mejor golpe ligero y golpe fuerte porque muchos luchadores no tienen "puños" o "piernas" como tal
-  L: "L", // Light: Golpe ligero
-  S: "S", // Strong: Golpe fuerte
-  R: "R", // Range: Golpe a distancia
-  G: "G", // Grab: Agarrar
-  P: "P", // Parry: Tratar de devolver ataque de distancia
-  DASH: "DASH",
-  TAUNT: "TAUNT",
+    P: "P", // punch
+    K: "K", // kick
+    S: "S", // slash
+    HS: "HS", // heavy slash
+    R: "R", // range
+    D: "D", // Dust, agarre si es hacia der o izq
+    DASH: "DASH",
+    TAUNT: "TAUNT",
 });
 
