7348af411f1a689059e031a23ac177dd001c6082
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-04-05 12:53:19 +0200

    Change Stance from string to enum

---
+++ src/dataTemplate/fighters/enums/Stance.js
@@ -0,0 +1,9 @@
+/**
+ * Estado del personaje de pie o agachado.
+ * @type {Readonly<{HIGH: string, LOW: string}>}
+ * @readonly
+ */
+export const Stance = Object.freeze({
+    HIGH: "HIGH",
+    LOW: "LOW"
+});
