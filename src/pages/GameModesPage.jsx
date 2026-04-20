49c669f93c23ddb2826c04f4a44e323e78f932ff
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-04-20 11:21:42 +0200

    Updade GameModesPage.jsx

--- src/pages/GameModesPage.jsx
+++ src/pages/GameModesPage.jsx
@@ -1,19 +1,48 @@
-import {Heading1, Heading2} from "../modules/Heading/Heading.jsx"
+import {Heading1, Heading2, Heading3} from "../modules/Heading/Heading.jsx"
+import PLink from "../modules/Link/PLink.jsx";
+
 export default function GameModesPage() {
     return (
         <>
             <Heading1 id="game-modes">Modos de juego</Heading1>
             <Heading2 id="versus">Versus</Heading2>
             <p>
-
+                El modo <b>Versus</b> permite a dos jugadores enfrentarse entre ellos en local multijugador.
             </p>
-            <Heading2 id="train">Entrenamiento</Heading2>
+            <Heading2 id="training">Entrenamiento</Heading2>
             <p>
-
+                El modo <b>Entrenamiento</b> permite a uno o dos jugadores entrar a una partida con modificadores
+                particulares. Este tipo de modificadores tienen el objetivo de facilitar la experimentación del
+                juego. Algunas modificaciones por defecto pueden ser:
+                <ul>
+                    <li>Tiempo infinito.</li>
+                    <li>Recuperar la salud completa tras finalizar un combo o tras estar un tiempo sin recibir
+                        daño.
+                    </li>
+                    <li>Medidores de recursos se rellenan al máximo tras consumirse para estar siempre disponibles.</li>
+                </ul>
+                Otras cosas que se pueden hacer es grabar secuencias de inputs para ser compartidas online, ser
+                guardadas
+                y reproducidas, aplicarlas al rival si es controlado por la máquina .
             </p>
-            <Heading2 id="arcade">Arcade/Historia</Heading2>
+            <Heading3 id="training-cpu">CPU de entrenamiento</Heading3>
             <p>
-
+                Cuando solo un jugador entra al entrenamiento, el rival será controlado por la máquina.
+                La máquina podrá tener distintos comportamientos accesibles desde el menú de pausa:
+                <ul>
+                    <li>Pasivo (Por defecto)</li>
+                    <li>Agachado</li>
+                    <li>Bloqueo [De pie, agachado, aleatorio, predecir]</li>
+                    <li>Repetir ataque: repite un ataque determinado siempre que puede.</li>
+                    <li>Repetir una secuencia de movimientos guardada.</li>
+                </ul>
+            </p>
+            <Heading2 id="arcade">Arcade</Heading2>
+            <p>
+                El modo <b>Arcade</b> es un modo de juego de un solo jugador en el que, según el luchador que se elija,
+                el jugador se enfrentará a una serie de retos y combates temáticos específicos al luchador elegido de la
+                misma forma que en <PLink href={"https://www.ssbwiki.com/Classic_Mode_(SSBU)"}>Classic Mode</PLink> en
+                Super Smash Bros Ultimate.
             </p>
         </>
     );
