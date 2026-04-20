8494c2925e76a6b23b461da9dc669ef102d7af12
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-04-20 11:44:07 +0200

    Fighter Peppemoldy added

--- src/data/fighter/fighters.jsx
+++ src/data/fighter/fighters.jsx
@@ -2,10 +2,12 @@ import {exampleFighter} from "./fighters/exampleFighter/exampleFighter.jsx";
 import {fighterBolognesa} from "./fighters/bolognesa/fighterBolognesa.jsx";
 import {fighterJohnGrinder} from "./fighters/johnGrinder/fighterJohnGrinder.jsx";
 import {fighterElChef} from "./fighters/elChef/fighterElChef.jsx";
+import {fighterPeppemoldy} from "./fighters/peppemoldy/fighterPeppemoldy.jsx";
 
 export const fighters = [
     exampleFighter,
     fighterJohnGrinder,
     fighterBolognesa,
     fighterElChef,
+    fighterPeppemoldy
 ]
---
+++ src/data/fighter/fighters/peppemoldy/fighterPeppemoldy.jsx
@@ -0,0 +1,47 @@
+import {Fighter} from "../../../../dataTemplate/fighters/Fighter.jsx";
+import {Archetype} from "../../../../dataTemplate/fighters/enums/Archetype.js";
+import {Source} from "../../../../dataTemplate/fighters/enums/Source.js";
+import {FighterType} from "../../../../dataTemplate/fighters/enums/FighterType.js";
+
+export const fighterPeppemoldy = new Fighter({
+    name: "Peppemoldy",
+    nickName: "Moldy Pep",
+    archetype: Archetype.BALANCE,
+    description: (fighter) => (
+        <>
+            No comerse el último trozo tiene consecuencias.
+            {fighter.name} es el resultado de no comerse la última porción de la comida.
+            Tras cobrar vida tras un tiempo en la basura, fue en ese contenedor común donde
+            encontró sus características gafas.
+        </>
+    ),
+    easyToUse: 4,
+    narrativeOrigin:
+        <>
+            Basura de un restaurante.
+        </>,
+    prosCons: {
+        pros: [
+            <>Versatil tanto de cerca como a distancia media.</>,
+            <>Muy versátil en el aire.</>
+        ],
+        cons: [
+            <>Lento en el suelo.</>
+        ]
+    },
+    type: FighterType.FOOD,
+    source: Source.BASE_GAME,
+    icon: "",
+    fullArt: "../../../public/fighters/fullart/peppemoldy_fullArt_temp.png",
+    portraitConfig: {
+        x: 0.6,
+        y: 0,
+        zoom: 1
+    },
+    baseStats: {
+        health: 100,
+        walkSpeed: 45,
+        runSpeed: 85
+    },
+    //moveSet:
+});
