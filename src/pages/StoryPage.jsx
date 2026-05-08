e3c69dc08311bc6730d636d7b4d3caeea47cb412
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-05-08 21:59:39 +0200

    Added alert

--- src/pages/StoryPage.jsx
+++ src/pages/StoryPage.jsx
@@ -1,54 +1,77 @@
 ﻿import {Heading1, Heading2} from "../modules/Heading/Heading.jsx";
+import AlertPanel from "../modules/AlertPanel/AlertPanel.jsx";
 
 export default function StoryPage() {
     return (
         <>
             <Heading1 id="story-page">Historia</Heading1>
+            <AlertPanel>
+                TODO: Los contenidos de esta página deben cumplir los requisitos.<br/>
+                historia a grandes rasgos: (planteamiento - nudo - desenlace). Como es un juego de peleas el conflicto
+                general de la comida mutante puede no resolverse.<br/>
+                Añadir escaleta
+            </AlertPanel>
             <Heading2 id="synopsis">Sinopsis</Heading2>
             <p>
-
-                La COMIDA ESTA MUTANDO!!, DEBEMOS DETERNERLA!!
-                Unos alimentos descontrolados están destruyendo nuestro planeta, para detenerlos necestitamos tu ayuda. La ayuda de los Couco!!
-
+                ¡La comida está mutando!
+                Unos alimentos descontrolados están destruyendo nuestro planeta, para detenerlos necesitamos tu ayuda.
+                <br/>
+                <b>¡La ayuda de los Couco!</b>
             </p>
-
             <p>
-
                 Elige un bando y enfrentate en una deliciosa batalla contra tus enemigos.
-                De ti depende quién se coronará como Rey de la Cadena Alimentaria!!
-
+                De ti depende quién se coronará como Rey de la <b>Cadena Alimenticia</b>
             </p>
 
             <Heading2 id="story-gamemoge-introduction">Introducción</Heading2>
             <p>
-                Desde hace décadas el gran crecimiento demográfico está asfixiando a la población, los recursos de nuestro planeta son muy limitados. Aunque podamos crear edificios más altos, el suelo es un gran problema.
-                Cada vez hay menos espacio y la contaminación que provoca la humanidad está perjudicando gravemente a la calidad de la tierra.
+                Desde hace décadas el gran crecimiento demográfico está asfixiando a la población, los recursos de
+                nuestro planeta son muy limitados. Aunque podamos crear edificios más altos, el suelo es un gran
+                problema.
+                Cada vez hay menos espacio y la contaminación que provoca la humanidad está perjudicando gravemente a la
+                calidad de la tierra.
                 <p>
-                    Para poder enfrentar la hambruna y el desabastecimiento ya no podíamos recurrir a los métodos tradicionales, debíamos poner nuestro foco en la biotecnología.
+                    Para poder enfrentar la hambruna y el desabastecimiento ya no podíamos recurrir a los métodos
+                    tradicionales, debíamos poner nuestro foco en la biotecnología.
                 </p>
 
                 <p>
-                    Debido a la creciente situación de emergencia tuvimos que dejar a un lado nuestra moral y explotar lo que ya tenemos.
+                    Debido a la creciente situación de emergencia tuvimos que dejar a un lado nuestra moral y explotar
+                    lo que ya tenemos.
                 </p>
 
                 <p>
-                    Logramos desarrollar alimentos de gran calidad en muy poco tiempo. Aumentamos la eficiencia de la producción y al fin parece que podemos enfrentar la crisis pero, todo tiene un precio.
+                    Logramos desarrollar alimentos de gran calidad en muy poco tiempo. Aumentamos la eficiencia de la
+                    producción y al fin parece que podemos enfrentar la crisis pero, todo tiene un precio.
                 </p>
                 <p>
-                    La manipulación del ADN y los nuevos químicos son demasiado inestables y requieren de procesos demasiado complejos para su uso efectivo. Esto provocó el “Despertar” de nuevos seres, nuevas abominaciones creadas por el hombre, deliciosas abominaciones…
+                    La manipulación del ADN y los nuevos químicos son demasiado inestables y requieren de procesos
+                    demasiado complejos para su uso efectivo. Esto provocó el “Despertar” de nuevos seres, nuevas
+                    abominaciones creadas por el hombre, deliciosas abominaciones…
                 </p>
                 <p>
-                    Como el “Despertar” podía surgir en cualquier momento y era imposible de prever. La única forma de contener la posible tragedia mientras se seguía investigando en métodos de producción fue crearnos a nosotros, los Cuoco, la nueva generación de Chefs.
+                    Como el “Despertar” podía surgir en cualquier momento y era imposible de prever. La única forma de
+                    contener la posible tragedia mientras se seguía investigando en métodos de producción fue crearnos a
+                    nosotros, los Cuoco, la nueva generación de Chefs.
                 </p>
 
                 <p>
-                    Los mejores Chefs teníamos que estar preparados para cualquier adversidad. Nos enseñaron las distintas artes del especiado, teoría avanzada del sabor, técnicas ancestrales evolutivas del corte, y más, mucho más, debíamos estar preparados ante todo.
+                    Los mejores Chefs teníamos que estar preparados para cualquier adversidad. Nos enseñaron las
+                    distintas artes del especiado, teoría avanzada del sabor, técnicas ancestrales evolutivas del corte,
+                    y más, mucho más, debíamos estar preparados ante todo.
                 </p>
                 <p>
-                    Estoy harto de la mentalidad tan pija y conservadora de los incompetentes de mis compañeros. Me niego a atascarme en la cocina tan arcaica que defienden, me da igual que las personas más ricas del mundo vayan a nuestro restaurante, tienen miedo a descubrir, tienen miedo a ver el nuevo mundo, lleno de sabores, texturas y experiencias únicas. Todo está a nuestro alcance, podemos crearlo todo. Pero dicen que el riesgo es muy alto y que los comensales prefieren comida menos “sintética”, ja, mi tortilla.
+                    Estoy harto de la mentalidad tan pija y conservadora de los incompetentes de mis compañeros. Me
+                    niego a atascarme en la cocina tan arcaica que defienden, me da igual que las personas más ricas del
+                    mundo vayan a nuestro restaurante, tienen miedo a descubrir, tienen miedo a ver el nuevo mundo,
+                    lleno de sabores, texturas y experiencias únicas. Todo está a nuestro alcance, podemos crearlo todo.
+                    Pero dicen que el riesgo es muy alto y que los comensales prefieren comida menos “sintética”, ja, mi
+                    tortilla.
                 </p>
                 <p>
-                    Me despidieron… No… ¡¡NO!! ¡¡YO LES DEJÉ, LES ABANDONÉ!!, ¡¡NO ENTIENDEN LA OPORTUNIDAD QUE ESTÁN DEJANDO PASAR!!, ¡¡LES DEMOSTRARÉ LA COCINA DEL FUTURO!!, ¡¡LES DEMOSTRARÉ DE QUÉ ESTOY HECHO!!, ¡¡VOY A ENCONTRAR EL SABOR PERFECTO EN ESTE MUNDO TAN SABROSO!!
+                    Me despidieron… No… ¡¡NO!! ¡¡YO LES DEJÉ, LES ABANDONÉ!!, ¡¡NO ENTIENDEN LA OPORTUNIDAD QUE ESTÁN
+                    DEJANDO PASAR!!, ¡¡LES DEMOSTRARÉ LA COCINA DEL FUTURO!!, ¡¡LES DEMOSTRARÉ DE QUÉ ESTOY HECHO!!,
+                    ¡¡VOY A ENCONTRAR EL SABOR PERFECTO EN ESTE MUNDO TAN SABROSO!!
                 </p>
 
             </p>
