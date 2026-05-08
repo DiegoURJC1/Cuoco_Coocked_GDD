a507ca1bea0540204800e765622baa9e774ccd55
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-05-09 00:37:59 +0200

    more adjustments

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -8,52 +8,51 @@ export default function TeamPage() {
             <p>
                 Seis alumnos de Fundamentos de la Jugabilidad son los programadores y artistas desarrolladores de
                 Cuoco Cooked.
-
-           </p>
-                    <table>
-    <tbody>
-        <tr>
-            <td><strong>Desarrollo web</strong></td>
-            <td>Diego</td>
-        </tr>
-        <tr>
-            <td><strong>Diseño de interfaces web</strong></td>
-            <td>Diego, Paula</td>
-        </tr>
-        <tr>
-            <td><strong>Diseño de personajes</strong></td>
-            <td>Diego, Chus, Carla, Fernando</td>
-        </tr>
-        <tr>
-            <td><strong>Diseño de escenarios y aderezos</strong></td>
-            <td>Diego, Chus, Carla</td>
-        </tr>
-        <tr>
-            <td><strong>Historia</strong></td>
-            <td>Fernando</td>
-        </tr>
-        <tr>
-            <td><strong>Diseño de interfaces del juego</strong></td>
-            <td>Carla, Paula</td>
-        </tr>
-        <tr>
-            <td><strong>Diseño y desarrollo de mecánicas</strong></td>
-            <td>Chus</td>
-        </tr>
-        <tr>
-            <td><strong>Música original</strong></td>
-            <td>Carla</td>
-        </tr>
-        <tr>
-            <td><strong>Tipografía original</strong></td>
-            <td>Chus</td>
-        </tr>
-        <tr>
-            <td><strong>Modelo de negocio</strong></td>
-            <td>Fernando, Paula</td>
-        </tr>
-    </tbody>
-</table>
+            </p>
+            <table>
+                <tbody>
+                <tr>
+                    <td><strong>Desarrollo web</strong></td>
+                    <td>Diego</td>
+                </tr>
+                <tr>
+                    <td><strong>Diseño de interfaces web</strong></td>
+                    <td>Diego, Paula</td>
+                </tr>
+                <tr>
+                    <td><strong>Diseño de personajes</strong></td>
+                    <td>Diego, Chus, Carla, Fernando</td>
+                </tr>
+                <tr>
+                    <td><strong>Diseño de escenarios y aderezos</strong></td>
+                    <td>Diego, Chus, Carla</td>
+                </tr>
+                <tr>
+                    <td><strong>Historia</strong></td>
+                    <td>Fernando</td>
+                </tr>
+                <tr>
+                    <td><strong>Diseño de interfaces del juego</strong></td>
+                    <td>Carla, Paula</td>
+                </tr>
+                <tr>
+                    <td><strong>Diseño y desarrollo de mecánicas</strong></td>
+                    <td>Chus</td>
+                </tr>
+                <tr>
+                    <td><strong>Música original</strong></td>
+                    <td>Carla</td>
+                </tr>
+                <tr>
+                    <td><strong>Tipografía original</strong></td>
+                    <td>Chus</td>
+                </tr>
+                <tr>
+                    <td><strong>Modelo de negocio</strong></td>
+                    <td>Fernando, Paula</td>
+                </tr>
+                </tbody>
+            </table>
 
 
             <img style={{
