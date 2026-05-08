6056ec56ded45ce75d3d5226669ee4007c9b5f17
paula <asrielissocute@gmail.com>
2026-05-08 21:35:39 +0200

    PNG fondos

--- src/pages/BusinessPage.jsx
+++ src/pages/BusinessPage.jsx
@@ -49,18 +49,33 @@ export default function BusinessPage() {
                 <AlertPanel>TODO: cuando se definan las fases del proyecto, poner cantidad de personajes y de escenarios
                 dentro de cada fase</AlertPanel>
 
-                | Fase            | Personajes       | Notas                                |
-                | --------------- | ---------------- | ------------------------------------ |
-                | **MVP**         | 4-6 personajes   | Cubriendo los arquetipos principales |30000
-                | **Lnzmnto Steam** | 8-12 personajes  | Roster completo con 1-2 humanos    |75100
-                | **Traduc**      | chino, japo      | Por definir                          |77600
-                | **GOG**         | drm free         | Por definir                          |87600
-                | **mods,workshop** | coreano y portugués   | Por definir                   |132500
-                | **epic games**      | arreglar parches, mantenmiento  | Por definir       |137500
-                | **DLC april foods**      | mecáicas nuevas      | Por definir             |170000
-                | **Switch**      |                  | Por definir                          |201000
-                | **Play 4, Play 5**      | nuevosescenarios y aderezos    | Por definir    |286000
+                En cuanto al presupuesto, iniciamos una recaudación en kickstarter para obtener datos con cifras
+                realistas del número de interesados en el juego para buscar un publisher.
+                Se ha estimado que se necesitarían unos 600.000€ para su lanzamiento en las plataformas
+                (Steam, Epic Games Store, GoG, Nintendo Switch) y publicación de diferentes DLC o actualizaciones.
+                El gráfico a continuación muestra las diferentes fases de producción en cuanto a objetivos superados
+                en recaudación (en caso de no contar con un publisher):
             </p>
+            <img style={{width: 'clamp(200px, 50%, 500px)',
+                transformOrigin: 'center', // CamelCase y en un solo objeto
+                display: 'block',           // Necesario para que el margen funcione
+                margin: '0 auto'            // Para centrarla
+                }} src={`${import.meta.env.BASE_URL}diagrams/tenedorRoadMapDiagram.png`}/>
+            {/*base_url=public*/}
+            <p>
+                El equipo, formado por seis programadores y artistas, recibiría un presupuesto de 20.000€ euros al
+                mes para trabajar en el proyecto. Se han tenido en cuenta los pagos a las diferentes plataformas para
+                publicar el juego en ellas o adquerir los kits de desarrollador. De igual manera se ha realizado un
+                pequeño estudio sobre la localización del videojuego, para presupuestar gastos en traducciones.
+                El rango de precio general está en torno a los 0,06€-0,10€ por palabra. Consideramos que la
+                traducción abarcará unas 9.000 palabras. Se establece un porcentaje del 25% para testing, lo que nos
+                lleva a unos 1.350 € por idioma. Usaremos Lockpick para las traducicciones a inglés, portugués, alemán,
+                chino, japonés, francés, italiano y ruso. En total se presupuestan 10.800€.
+            </p>
+
+
+            {/*base_url=public
+
             <Heading2 id="roadmap">Roadmap</Heading2>
             <p>
                 Futuras expansiones asociadas a hitos económicos
@@ -72,7 +87,7 @@ export default function BusinessPage() {
             <Heading2 id="launch">Lanzamiento</Heading2>
             <p>
                 Plataformas, fechas estimadas, tamaño roster, cantidad escenarios, modos incluidos ...
-            </p>
+            </p>*/}
         </>
     );
 }
